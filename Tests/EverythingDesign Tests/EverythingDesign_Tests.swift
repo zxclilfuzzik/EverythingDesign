272e54fe1c9cde83396abc96c0f0d0bd9612467f
lilfuzzik <lilfuzzik@vk.com>
2023-01-14 12:56:13 +0300

    settings

--- Tests/EverythingDesign Tests/EverythingDesign_Tests.swift
+++ Tests/EverythingDesign Tests/EverythingDesign_Tests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import EverythingDesign_v1
+@testable import EverythingDesign
 
-final class EverythingDesign_v1Tests: XCTestCase {
+final class EverythingDesign_Tests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
