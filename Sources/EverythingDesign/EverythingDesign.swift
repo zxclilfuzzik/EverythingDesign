0b4a228d8f98df2d5c039eaa196b4e16075b5397
lilfuzzik <lilfuzzik@vk.com>
2023-01-14 13:15:14 +0300

    c

--- Sources/EverythingDesign/EverythingDesign.swift
+++ Sources/EverythingDesign/EverythingDesign.swift
@@ -1,10 +1,32 @@
 import UIKit
 
 let widths = UIWindow().screen.bounds.width * 0.95
+import SwiftUI
 
-
-struct EverythingDesign_v1 {
+public struct EverythingDesign {
     //    public private(set) var text = "Hello, World!"
     
-    init() {}
+    public init() {}
 }
+
+public struct UIAlertText: View {
+    
+    var color: Color
+    var text: String
+    
+    public init(color: Color, text: String) {
+        self._color = color
+        self._text = text
+    }
+    
+    public var body: some View {
+        HStack {
+            Text(self.text)
+                .foregroundColor(self.color)
+                .fontWeight(.medium)
+                .font(.system(size: 10))
+            Spacer()
+        }
+    }
+}
+
