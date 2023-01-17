0570f943f546c8621a46adadd4f80e00077ae000
lilfuzzik <lilfuzzik@vk.com>
2023-01-17 12:01:43 +0300

    hehehe

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(
             name: "EverythingDesign",
             type: .dynamic,
-            targets: ["EverythingDesign"]),
+            targets: ["EverythingDesign"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
--- Sources/EverythingDesign/EverythingDesign.swift
+++ Sources/EverythingDesign/EverythingDesign.swift
@@ -30,3 +30,27 @@ public struct UIAlertText: View {
     }
 }
 
+public struct UINavigationBar: View {
+    
+    
+    var title: String
+    var icon: String
+    
+    public init(title: String, icon: String) {
+        self.title = title
+        self.icon = icon
+    }
+    
+    public var body: some View {
+        HStack(spacing: 0) {
+            Text(title)
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                .bold()
+            Spacer()
+            Button(action: {}, label: {Image(systemName: icon).foregroundColor(.white)})
+        }
+    }
+    
+}
+
