673f87f34964ba052afebb2ab0ea957b4af84fbe
lilfuzzik <lilfuzzik@vk.com>
2023-01-29 00:04:49 +0300

    add alert

--- Sources/UIAlert/EverythingDesignAlert.swift
+++ Sources/UIAlert/EverythingDesignAlert.swift
@@ -20,7 +20,7 @@ public struct UIAlertText: View {
             Text(self.text)
                 .foregroundColor(self.color)
                 .fontWeight(.medium)
-                .font(.system(size: 10))
+                .font(.system(size: 12))
             Spacer()
         }
     }
@@ -30,37 +30,21 @@ public struct UIAlertText: View {
 //Adding test UIAlert
 public struct UIAlertWindow: View {
     
-    var text: String
-    var btnText: String
-    var action: () -> Void
-    var color: Color
-    
-    public init(text: String, btnText: String, action: @escaping () -> Void, color: Color) {
-        self.text = text
-        self.btnText = btnText
-        self.action = action
-        self.color = color
-    }
+    var alertText: String
     
     public var body: some View {
-        ZStack {
-            Color.black
-                .ignoresSafeArea()
-            VStack(spacing: 0) {
-                Text(text)
-                Divider()
-                    .frame(width: 269)
-                    .foregroundColor(.gray)
-                Button(action: action) {
-                    Text(btnText)
-                        .bold()
-                        .foregroundColor(color)
-                }
+        VStack(spacing: 0) {
+            ZStack {
+                Color.green
+            }
+            HStack {
+                Text(alertText)
+                    .foregroundColor(.white)
+                    .fontWeight(.medium)
+                    .font(.system(size: 15))
             }
         }
-        .frame(width: widths, height: 178)
-        .cornerRadius(20)
+        .frame(width: widths)
     }
-    
 }
 // // //
